af5016c83044eac4a6606018cad3fe9fe15c81b8
gustavo867 <tartatavo3@gmail.com>
2021-04-19 23:35:46 -0300

    :lipstick: global styles

---
+++ plant-manager/src/styles/colors.types.ts
@@ -0,0 +1,15 @@
+export interface Theme {
+  green: string;
+  green_dark: string;
+  green_light: string;
+  heading: string;
+  body_dark: string;
+  body_light: string;
+  background: string;
+  shape: string;
+  white: string;
+  gray: string;
+  blue: string;
+  blue_light: string;
+  red: string;
+}
